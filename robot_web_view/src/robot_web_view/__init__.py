c82150fb975711c12d58c0221915afc342a3e622
Nick Hawes <nickh@robots.ox.ac.uk>
2018-06-18 09:53:18 +0100

    Added template parameter for image stream host

--- robot_web_view/src/robot_web_view/__init__.py
+++ robot_web_view/src/robot_web_view/__init__.py
@@ -5,14 +5,14 @@ import roslib
 template_dir = roslib.packages.get_pkg_dir('robot_web_view') + '/templates'
 render = web.template.render(template_dir)
 
-def create_robot_view_page(full_path, image_topic, other_button=''):		
+def create_robot_view_page(full_path, image_topic, other_button='', image_host='localhost'):		
 	""" Generates the tweet me page at the given location. """
 	try:
 		os.makedirs(os.path.dirname(full_path))
 	except OSError, e:
 		pass
 
-	page = str(render.robot_view(image_topic, other_button))
+	page = str(render.robot_view(image_topic, other_button, image_host))
 	with open(full_path, 'w+') as f:
 		f.write(page)
 
